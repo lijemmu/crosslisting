ede956e68114907e6f086ad2c49745a781774ef0
Emmanuel Kebede <emmanuelemrias@gmail.com>
2022-07-29 12:07:54 -0400

    saving changes

--- flasksite/__init__.py
+++ flasksite/__init__.py
@@ -4,7 +4,7 @@ from flask_behind_proxy import FlaskBehindProxy
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-import config
+# import config
 
 
 def create_app(test_config=None):
@@ -13,6 +13,8 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         SECRET_KEY='063b5d59f24fbf66d126cfb5e661902f',
+        SQLALCHEMY_DATABASE_URI='postgresql://zghuakjjjmaizw:e7eb618d0d9815a8c487ec7b5505cb5200bc079fa1516dfc1d61f9daafb57e77@ec2-44-206-197-71.compute-1.amazonaws.com:5432/d9jqgsjfb7ptd5',
+
         # SQLALCHEMY_DATABASE_URI='sqlite:///site.db',
         # GITHUB_CLIENT_ID=config.CLIENT_ID,
         # GITHUB_CLIENT_SECRET=config.CLIENT_SECRET
