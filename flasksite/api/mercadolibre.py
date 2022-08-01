dcf6689ead43112385045b1603c799ec9c68ec49
Fernanda Pisconte <fpisconte@berkeley.edu>
2022-08-01 16:35:37 -0500

    Mercado libre api, create posting

--- flasksite/api/mercadolibre.py
+++ flasksite/api/mercadolibre.py
@@ -41,39 +41,31 @@ def find_category(title):
 
     return category_id
 
-def attributes_required(category_id):
+def attributes_required(category_id): 
 
     headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN}
     url = "https://api.mercadolibre.com/categories/" + category_id + "/attributes"
     resp = requests.get(url, headers=headers)
-    pprint.pprint(resp.json())
+    print(resp.json())
 
-def post_listing(title, description, price, quantity,condition, warranty_time):
+
+def post_listing_tech(title, description, price, quantity,condition, warranty_time, brand, line, model, color, os, processor):
 
     #INFO NEEDED
     '''
-    Título
-    Descripción
-    Estado
-    Cantidad disponible
-    Imágenes
-    Categoría
-    Precio
-    Moneda
-    Métodos de pago
-    Envío
-    Identificadores de productos
-    SKU
-    Variaciones
-    Tipos de publicación
-    Garantía del producto
+    *BRAND: 
+    *LINE
+    *MODEL
+    *COLOR
+    *OS_NAME
+    *PROCESSOR_BRAND
     '''
 
     category_id = find_category(title)
 
     body = {
     "title": title,
-    "category_id": category_id,
+    "category_id": "MPE1652",
     "price":price,
     "currency_id":"PEN",
     "available_quantity":quantity,
@@ -108,42 +100,114 @@ def post_listing(title, description, price, quantity,condition, warranty_time):
     "attributes":[
         {
             "id":"BRAND",
-            "value_name":"Rolex"
+            "value_name": brand
         },
         {
             "id":"MODEL",
-            "value_name":"6912"
+            "value_name": model
+        },
+        
+        {
+            "id":"COLOR",
+            "value_name": color
         },
         {
-            "id":"GENDER",
-            "value_name":"Mujer"
+            "id":"LINE",
+            "value_name": line
         },
         {
-            "id":"STRAP_COLOR",
-            "value_name":"Plateado"
+            "id":"OS_NAME",
+            "value_name": os
         },
         {
-            "id":"BEZEL_COLOR",
-            "value_name":"Plateado"
-        },
-        {
-            "id":"BACKGROUND_COLOR",
-            "value_name":"Verde"
-        },
-        {
-            "id":"CASE_COLOR",
-            "value_name":"Plateado"
+            "id":"PROCESSOR_BRAND",
+            "value_name": processor
         }
 
+    ],
+    }
+
+    response = listing_call(body)
+    listing_id = response[0]['id']
+    add_description(listing_id, description)
+
+def post_listing_clothes(title, description, price, quantity,condition, warranty_time, brand, color, size):
+
+    #INFO NEEDED
+    '''
+    *BRAND: 
+    *LINE
+    *MODEL
+    *COLOR
+    *OS_NAME
+    *PROCESSOR_BRAND
+    '''
+
+    category_id = find_category(title)
+
+    body = {
+    "title": title,
+    "category_id": "MPE6585",
+    "price":price,
+    "currency_id":"PEN",
+    "available_quantity":quantity,
+    "buying_mode":"buy_it_now",
+    "condition": condition,
+    "listing_type_id":"gold_special",
+    "sale_terms":[
+        {
+            "id":"WARRANTY_TYPE",
+            "value_name":"Garantía del vendedor"
+        },
+        {
+            "id":"WARRANTY_TIME",
+            "value_name": warranty_time
+        }
+    ],
+    "pictures":[
+        {
+            "source":"https://content.rolex.com//dam/2022/upright-cc/m126200-0020.png"
+        },
+        {
+            "source":"https://content.rolex.com//dam/2022/laying-ba-with-shadow/m126200-0020.png"
+        },
+        {
+            "source":"https://content.rolex.com//dam/2022/presentation-box-hr/m126200-0020.jpg"
+        },
+        {
+            "source":"https://content.rolex.com//dam/2022/presentation-tray/m126200-0020.jpg"
+        }
+    ],
+
+    "attributes":[
+        {
+            "id":"BRAND",
+            "value_name": brand
+        },
+        
+        {
+            "id":"COLOR",
+            "value_name": color
+        },
+
+        {
+            "id":"SIZE",
+            "value_name": size
+        },
     ]
     }
+
+    response = listing_call(body)
+    listing_id = response['id']
+    add_description(listing_id, description)
+
+
+def listing_call(body):
 
     headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN, 'Content-Type': 'application/json'}
     url = "https://api.mercadolibre.com/items"
     response = requests.request("POST", url, headers=headers, data=json.dumps(body))
-    print(response.json())
-    #Updating with the description
-    #print(add_description(response['id']), description)
+    return response.json()
 
 def add_description(product_id, description):
 
@@ -159,7 +223,16 @@ def add_description(product_id, description):
     return response.json()
 
 
+#prepare_body_tech("MacBook pro 2021 Apple", "New, ready to use", 6000, 3,"new", "6 meses", "Apple", "MacBook", "2021", "Black", "macOS", "Apple")
+
+
+
+
 #post_listing()
 #add_description("MPE615787203")
 #post_listing("MacBook Pro 16'", "MacBook nueva, no usada, modelo 2021", "6000", "2", "new", "6 meses")
-attributes_required("MPE1652")
+#attributes_required("MPE1652")
+#print(find_category("MacBook nueva, no usada, modelo 2021"))
+#print(find_category("Zapatillas nike 2022 nuevas"))
+#attributes_required("MPE6585")
+post_listing_clothes("Zapatillas Nike 2022", "Modelo nuevo, nuevo de fabrica", 600, 1, "new", "6 meses", "Nike", "blanco", "M")
